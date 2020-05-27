ebcc06c69f28b0c2c1c45aa65cac7c636eb1dd48
inity8 <di848588@gmail.com>
2020-05-27 22:56:31 +0300

    add links

--- script.py
+++ script.py
@@ -1,12 +1,20 @@
 import scrapy
 
-class BlogSpider(scrapy.Spider):
-    name = 'blogspider'
-    start_urls = ['https://blog.scrapinghub.com']
+class Spider(scrapy.Spider):
+    name = 'spider'
+    start_urls = ['https://auto.ru/cars/tesla/all/']
 
     def parse(self, response):
-        for title in response.css('.post-header>h2'):
-            yield {'title': title.css('a ::text').get()}
+        for car_div in response.css('a.ListingCars-module__container ListingCars-module__list'):
+            link = car_div.css('a.Link ListingItemTitle-module__link').get()
+            title = link.css('::text').get()
+            href = link.css('::attr(href)').get()
 
-        for next_page in response.css('a.next-posts-link'):
-            yield response.follow(next_page, self.parse)
+
+            yield {
+                'title': title,
+                'href': href,
+            }
+        #
+        # for next_page in response.css('a.next-posts-link'):
+        #     yield response.follow(next_page, self.parse)
